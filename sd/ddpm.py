f1206e72bd5a3d2792b81d10c5ed0730c524ef6e
Mafaz03 <mohdmafaz200303@gmail.com>
2024-09-30 22:09:30 +0530

    ddpm building

--- sd/ddpm.py
+++ sd/ddpm.py
@@ -19,4 +19,30 @@ class DDPMSampler:
         timesteps_ratio = n_inference_steps // self.num_training_steps
 
         self.timesteps = torch.from_numpy((np.arange(0, n_inference_steps)*timesteps_ratio).round()[::-1].copy().astype(np.int64))
+
+    def add_noise(self, original_samples: torch.FloatTensor, timesteps: torch.IntTensor) -> torch.FloatTensor : 
         
+        alpha_cumprod = self.alpha_cumprod.to(device=original_samples.device, dtype=original_samples.dtype)
+        timesteps = timesteps.to(device=original_samples.device)
+
+        # var is alphacum prod
+        sqrt_alpha_prod = alpha_cumprod[timesteps] ** 0.5
+        sqrt_alpha_prod = sqrt_alpha_prod.flatten()
+
+        while len(sqrt_alpha_prod.shape) < len(original_samples.shape):
+            sqrt_alpha_prod = sqrt_alpha_prod.unsqueeze(-1)
+        
+        mean = sqrt_alpha_prod * original_samples
+        
+        sqrt_one_minus_sqrt_alpha_prod = (1 - sqrt_alpha_prod) ** 0.5 # Basically var 
+
+        while len(sqrt_one_minus_sqrt_alpha_prod.shape) < len(original_samples.shape):
+            sqrt_one_minus_sqrt_alpha_prod = sqrt_one_minus_sqrt_alpha_prod.unsqueeze(-1)
+        
+        var = sqrt_one_minus_sqrt_alpha_prod
+        
+        noise = torch.randn(original_samples.shape, generator=self.generator, dtype=original_samples.dtype)
+        noisy_samples =  mean + var * noise
+
+        return noisy_samples
+
